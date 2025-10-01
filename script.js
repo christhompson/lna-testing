eb652df9131060aadc5481fbbd5eff23cd099422
Chris Thompson <chris.j.thompson@gmail.com>
2025-10-01 23:32:43 +0000

    Add frame.html test page and add main frame navigation case

--- script.js
+++ script.js
@@ -190,3 +190,49 @@ permissionsApiQueryButton.onclick = (event) => {
             appendPermissionsApiResult(`error = ${error}`);
         });
 };
+
+// Main frame navigation
+
+const mainframeForm = document.getElementById("mainframe-form");
+const mainframeInputBox = document.getElementById("mainframe-input-box");
+
+mainframeForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!mainframeInputBox.validity.valid) {
+        console.log("Input URL not valid");
+        return;
+    }
+    console.log("Navigating page to " + mainframeInputBox.value);
+    window.location = mainframeInputBox.value;
+});
+
+
+// Used for frame.html subframe navigation.
+
+const subframeFormNoAllowlist = document.getElementById("subframe-form-not-allowlisted");
+const subframeInputBoxNoAllowlist = document.getElementById("subframe-input-box-not-allowlisted");
+const subframeIFrameNoAllowlist = document.getElementById("subframe-not-allowlisted");
+
+subframeFormNoAllowlist.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!subframeInputBoxNoAllowlist.validity.valid) {
+        console.log("Input URL not valid");
+        return;
+    }
+    console.log("Navigating subframe to " + subframeInputBoxNoAllowlist.value);
+    subframeIFrameNoAllowlist.src = subframeInputBoxNoAllowlist.value;
+});
+
+const subframeFormAllowlist = document.getElementById("subframe-form-allowlisted");
+const subframeInputBoxAllowlist = document.getElementById("subframe-input-box-allowlisted");
+const subframeIFrameAllowlist = document.getElementById("subframe-allowlisted");
+
+subframeFormAllowlist.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!subframeInputBoxAllowlist.validity.valid) {
+        console.log("Input URL not valid");
+        return;
+    }
+    console.log("Navigating subframe to " + subframeInputBoxAllowlist.value);
+    subframeIFrameAllowlist.src = subframeInputBoxAllowlist.value;
+});
